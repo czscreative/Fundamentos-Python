bdd03e22d0d41e9ee20f50c71ec2a3f0ff921922
Cesar Augusto de Jesus <32915912+czscreative@users.noreply.github.com>
2026-07-27 21:23:11 -0300

    commit

--- Exercicios_4/exerc_4-13.py
+++ Exercicios_4/exerc_4-13.py
@@ -1,3 +1,6 @@
+# No programa a seguir, inverta as linhas do if e else, negando a condição.
+# Adicione as linhas necessárias para fazê-lo funcionar em Python.
+
 a = 35
 b = 28
 
---
+++ Exercicios_4/exerc_4-14.py
@@ -0,0 +1,11 @@
+# Reescreva o programa a seguir com if-elif-else.
+# Adicione as linhas necessárias para fazê-lo funcionar em Python.
+
+a = 15
+
+if a < 10:
+    print("a é menor que 10")
+elif a >= 10 and a < 20:
+    print("a é maior que 10 ne menor que 20")
+else:
+    print("a é maior que 20")
